45d15381ae5c5cdfbf931b003079021a80b8029d
Dmitry1512 <dima.kirshchin1512@gmail.com>
2024-07-21 21:06:15 +0300

    mobi1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 //scrooll effects
 AOS.init();
+
+
+//mobile navi
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.nav')
+burger.addEventListener('click',()=>{
+  burger.classList.toggle('active')
+  nav.classList.toggle('open')
+})
